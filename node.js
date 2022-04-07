ec7ccd4e3df01df12258e8da105a348a3d7b8452
KeshavAbhishek <crystaled2003@gmail.com>
2022-04-07 18:20:02 +0530

    Added Background Color, Reload Function Added

--- node.js
+++ node.js
@@ -1,16 +1,16 @@
 if(window.innerWidth<=700){
-    document.getElementById('circularFrame').style.background=`url('https://picsum.photos/${parseInt(window.innerWidth*0.7)}')`;
+    document.getElementById('circularFrame').style.backgroundImage=`url('https://picsum.photos/${parseInt(window.innerWidth*0.7)}')`;
 }
 else{
-    document.getElementById('circularFrame').style.background=`url('https://picsum.photos/${parseInt(window.innerWidth*0.3)}')`;
+    document.getElementById('circularFrame').style.backgroundImage=`url('https://picsum.photos/${parseInt(window.innerWidth*0.3)}')`;
 }
 
 window.addEventListener('resize',()=>{
     if(window.innerWidth<=699){
-        document.getElementById('circularFrame').style.background=`url('https://picsum.photos/${parseInt(window.innerWidth*0.7)}')`;
+        document.getElementById('circularFrame').style.backgroundImage=`url('https://picsum.photos/${parseInt(window.innerWidth*0.7)}')`;
     }
     else{
-        document.getElementById('circularFrame').style.background=`url('https://picsum.photos/${parseInt(window.innerWidth*0.3)}')`;
+        document.getElementById('circularFrame').style.backgroundImage=`url('https://picsum.photos/${parseInt(window.innerWidth*0.3)}')`;
     }
 })
 
