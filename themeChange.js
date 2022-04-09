a03a1d8df442f434131e5582a031db898d21e8da
KeshavAbhishek <crystaled2003@gmail.com>
2022-04-09 10:34:31 +0530

    Error Fix

--- themeChange.js
+++ themeChange.js
@@ -2,19 +2,19 @@ var changeTheme = "DAY";
 
 document.getElementById('themeFrame').addEventListener('click',()=>{
     if(changeTheme=="DAY"){
-        document.querySelector('#themeFrame div').classList.add('moveY')
-        document.querySelector('#themeFrame div').classList.remove('moveYOpp')
+        document.querySelector('#themeFrame div').classList.add('moveY');
+        document.querySelector('#themeFrame div').classList.remove('moveYOpp');
         document.getElementById('container').classList.add('changeMainBg');
         document.getElementById('container').classList.remove('changeMainBgBack');
         document.getElementById('digitalClock').classList.add('digitalClockChange');
-        changeTheme = "NIGHT"
+        changeTheme = "NIGHT";
     }
     else{
-        document.querySelector('#themeFrame div').classList.remove('moveY')
-        document.querySelector('#themeFrame div').classList.add('moveYOpp')
+        document.querySelector('#themeFrame div').classList.remove('moveY');
+        document.querySelector('#themeFrame div').classList.add('moveYOpp');
         document.getElementById('container').classList.remove('changeMainBg');
         document.getElementById('container').classList.add('changeMainBgBack');
         document.getElementById('digitalClock').classList.remove('digitalClockChange');
-        changeTheme = "DAY"
+        changeTheme = "DAY";
     }
 });
