ce4d5d448326460841ce9c3ec020785b41dfbb86
KeshavAbhishek <crystaled2003@gmail.com>
2022-04-12 09:48:23 +0530

    12-04-2022 Updated

--- timeUpdater.js
+++ timeUpdater.js
@@ -1,56 +1,4 @@
-// setInterval(() => {
-//     var currTime = new Date();
-//     currentTicks = Math.floor(currTime.getMinutes()/12);
-//     if(currTime.getHours()>12){
-//         document.getElementById('hourHand').style.transform=`rotate(-90deg) rotate(${(((currTime.getHours()-12)/12)*360)+(currentTicks)*6}deg)`;
-//         document.getElementById('hourHand').style.transition='all 1s ease-in-out';
-
-//         if(currTime.getHours()-12<10){
-//             document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`0${currTime.getHours()-12}`;
-//         }
-//         else{
-//             document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`${currTime.getHours()-12}`;
-//         }
-//         // document.querySelector('#digitalClock span:nth-child(4)').innerHTML='PM';
-//     }
-//     else{
-//         document.getElementById('hourHand').style.transform=`rotate(-90deg) rotate(${((currTime.getHours()/12)*360)+(currentTicks)*6}deg)`;
-//         document.getElementById('hourHand').style.transition='all 1s ease-in-out';
-
-//         if(currTime.getHours()<10){
-//             if(currTime.getHours()===0){
-//                 document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`12`;
-//             }
-//             else{
-//                 document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`0${currTime.getHours()}`;
-//             }
-//         }
-//         else{
-//             document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`${currTime.getHours()}`;
-//         }
-//         document.querySelector('#digitalClock span:nth-child(4)').innerHTML='AM';
-//     }
-//     if(currTime.getHours()>=12){document.querySelector('#digitalClock span:nth-child(4)').innerHTML='PM';}
-
-//     document.getElementById('minuteHand').style.transform=`rotate(-90deg) rotate(${(currTime.getMinutes()/60)*360}deg)`;
-//     document.getElementById('minuteHand').style.transition='all 1s ease-in-out';
-
-//     document.getElementById('secondHand').style.transform=`rotate(-90deg) rotate(${(currTime.getSeconds()/60)*360}deg)`;
-
-//     if(currTime.getSeconds()<10){
-//         document.querySelector('#digitalClock span:nth-child(3)').innerHTML=`0${currTime.getSeconds()}`;
-//     }
-//     else{
-//         document.querySelector('#digitalClock span:nth-child(3)').innerHTML=`${currTime.getSeconds()}`;
-//     }
-
-//     if(currTime.getMinutes()<10){
-//         document.querySelector('#digitalClock span:nth-child(2)').innerHTML=`0${currTime.getMinutes()}`;
-//     }
-//     else{
-//         document.querySelector('#digitalClock span:nth-child(2)').innerHTML=`${currTime.getMinutes()}`;
-//     }
-// }, 1000);
+var timeStamps = {0:"time12",1:"time1",2:"time2",3:"time3",4:"time4",5:"time5",6:"time6",7:"time7",8:"time8",9:"time9",10:"time10",11:"time11",12:"time12"};
 
 
 setInterval(() => {
@@ -72,13 +20,28 @@ setInterval(() => {
 
     if(currTime.getHours()>12){
         document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`${currTime.getHours()-12}`;
+        document.getElementById(timeStamps[currTime.getHours()-12-1]).classList.remove('hourHandOn');
+        document.getElementById(timeStamps[currTime.getHours()-12]).classList.add('hourHandOn');
     }
     else{
         if(currTime.getHours()<10){
             document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`0${currTime.getHours()}`;
+            // document.getElementById(timeStamps[currTime.getHours()-1]).classList.remove('hourHandOn');
+            // document.getElementById(timeStamps[currTime.getHours()]).classList.add('hourHandOn');
+            if(currTime.getHours()>=1){
+                document.getElementById(timeStamps[currTime.getHours()-1]).classList.remove('hourHandOn');
+                document.getElementById(timeStamps[currTime.getHours()]).classList.add('hourHandOn');
+            }
+            if(currTime.getHours()==0){
+                document.querySelector('#digitalClock span:nth-child(1)').innerHTML='12';
+                document.getElementById(timeStamps[11]).classList.remove('hourHandOn');
+                document.getElementById(timeStamps[12]).classList.add('hourHandOn');
+            }
         }
         else{
         document.querySelector('#digitalClock span:nth-child(1)').innerHTML=`${currTime.getHours()}`;
+        document.getElementById(timeStamps[currTime.getHours()-1]).classList.remove('hourHandOn');
+        document.getElementById(timeStamps[currTime.getHours()]).classList.add('hourHandOn');
         }
     }
 
@@ -93,3 +56,16 @@ setInterval(() => {
         document.querySelector('#digitalClock span:nth-child(4)').innerHTML='AM';
     }
 }, 1000);
+
+// time12
+// time6
+// time9
+// time3
+// time10
+// time4
+// time11
+// time5
+// time7
+// time1
+// time8
+// time2
